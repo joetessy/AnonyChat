0e54768667da4fabe0ddea884e97499983896393
Joe Tessy <josephctessy@gmail.com>
2017-08-15 15:31:11 -0400

    Websocket relays messages to new clients

--- websockets-server.js
+++ websockets-server.js
@@ -6,11 +6,16 @@ var ws = new WebSocketServer({
   port: port
 });
 
+let messages = [];
+
 console.log('websocket server started');
+
 ws.on('connection', (socket) => {
+  messages.forEach((message) => socket.send(message));
   console.log('client connection established');
   socket.on('message', (data) => {
     console.log('message received: ' + data);
-    socket.send(data);
+    messages.push(data);
+    ws.clients.forEach((clientSocket) => clientSocket.send(data));
   });
 });
